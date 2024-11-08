13d6a548b35e8a4878d419665c9a27a70428790e
imlyzh <enterindex@gmail.com>
2024-11-08 16:10:42 +0800

    add two string operator

--- src/object_operator.rs
+++ src/object_operator.rs
@@ -597,3 +597,26 @@ pub unsafe fn int2char(obj: Slot) -> Slot {
     // todo: alloc
     todo!()
 }
+
+/// String
+
+pub unsafe fn make_string(k: Slot) -> Slot {
+    assert_eq!(get_tag(k), ObjectTag::Number);
+    let ptr = k as *const Number;
+    let value = (*ptr).value as u8;
+    let _v = Char { head: ObjectHead { tag: ObjectTag::Char, moved: false }, value };
+    // todo: alloc
+    todo!()
+}
+
+pub unsafe fn make_string_with_fill(k: Slot, char: Slot) -> Slot {
+    assert_eq!(get_tag(k), ObjectTag::Number);
+    assert_eq!(get_tag(char), ObjectTag::Char);
+    let number_ptr = k as *const Number;
+    let char_ptr = char as *const Char;
+    let number= (*number_ptr).value;
+    let char = (*char_ptr).value;
+    // todo: alloc
+    // let _v = String{ head: ObjectHead { tag: ObjectTag::Char, moved: false }, value };
+    todo!()
+}

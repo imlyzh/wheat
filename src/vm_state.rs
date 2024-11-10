e675437ff464d43ace2ec80b1ca0698e9fa13f0a
imlyzh <enterindex@gmail.com>
2024-11-10 08:39:52 +0800

    add new_scope to VMState

--- src/vm_state.rs
+++ src/vm_state.rs
@@ -1,8 +1,10 @@
 use std::ptr::NonNull;
 
-use crate::{memory_manage::SemiSpaceMemory, object_model::Slot, scope_model::Scope};
-
-
+use crate::{
+    memory_manage::SemiSpaceMemory,
+    object_model::{ObjectHead, ObjectTag, Slot, Symbol},
+    scope_model::Scope,
+};
 
 #[derive(Debug, Clone)]
 pub struct VMState {
@@ -13,5 +15,20 @@ pub struct VMState {
 impl VMState {
     pub unsafe fn alloc(&mut self, size: usize) -> Slot {
         self.heap.alloc(self.current, size).as_mut()
+    }
+    pub unsafe fn new_scope(&mut self, variable: Slot, name: Option<Symbol>) -> Self {
+        let scope = self.alloc(std::mem::size_of::<Scope>());
+        let scope_ref = &mut (*(scope as *mut Scope));
+        scope_ref.head = ObjectHead {
+            tag: ObjectTag::Opaque,
+            moved: false,
+        };
+        scope_ref.name = name;
+        scope_ref.pointer = variable;
+        scope_ref.prev = Some(self.current);
+        Self {
+            heap: self.heap.clone(),
+            current: NonNull::new(scope as *mut Scope).unwrap_unchecked(),
+        }
     }
 }

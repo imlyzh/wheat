e0a0de277648dec499162dea4221a103c4d5c343
imlyzh <enterindex@gmail.com>
2024-11-11 15:05:15 +0800

    change semispace limit to 4k

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::{
 };
 
 use wheat::{
-    memory_manage::{page_size::_64K, SemiSpaceMemory},
+    memory_manage::{page_size::_4K, SemiSpaceMemory},
     object_operator::read::read_,
     vm_state::VMState,
 };
@@ -13,7 +13,7 @@ fn main() {
     println!("wheat programming language. Copyright 2024 lyzh");
     unsafe {
         let mut vms = VMState {
-            heap: SemiSpaceMemory::init(_64K, false),
+            heap: SemiSpaceMemory::init(_4K, false),
             current: None,
             symbol_cache: HashSet::new(),
         };

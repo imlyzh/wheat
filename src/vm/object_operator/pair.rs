ef1b5c60ff734fe00ae90e34e460321fed482641
imlyzh <enterindex@gmail.com>
2024-12-02 13:57:59 +0800

    remove need heap alloc

--- src/vm/object_operator/pair.rs
+++ src/vm/object_operator/pair.rs
@@ -5,8 +5,6 @@ use super::tageq::{eq, eqv};
 
 /// # pair
 
-// TODO: cons
-/// need heap alloc
 pub unsafe fn cons(vms: &mut VMState, obj0: Slot, obj1: Slot) -> Slot {
     make_pair(vms, obj0, obj1)
 }
@@ -39,12 +37,6 @@ pub unsafe fn set_cdr(pair: Slot, obj: Slot) {
     (*ptr).cdr = obj;
 }
 
-/// need heap alloc
-#[inline]
-pub unsafe fn list(objs: &[Slot]) -> Slot {
-    todo!()
-}
-
 #[inline]
 pub unsafe fn raw_length(list: Slot) -> usize {
     if get_tag(list) == ObjectTag::Null {
@@ -56,18 +48,6 @@ pub unsafe fn raw_length(list: Slot) -> usize {
         return 1;
     }
     raw_length(next) + 1
-}
-
-/// need heap alloc number
-#[inline]
-pub unsafe fn length(list: Slot) -> Slot {
-    let len = raw_length(list);
-    todo!()
-}
-
-/// need heap alloc
-pub unsafe fn append(list0: Slot, list1: Slot) -> Slot {
-    todo!()
 }
 
 pub unsafe fn memq(obj: Slot, list: Slot) -> Slot {
